f4f2909cea2ac5287ce571ae737dbecae1436dcc
jyooni <berry3891@naver.com>
2025-02-28 17:34:17 +0900

    feat: button 컴포넌트 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Button from '~/components/common/button';
+
 export default function Home() {
   return (
     <div>
-      <p>루트 페이지</p>
+      <Button text="BUTTON" size={'sm'} />
+      <Button text="BUTTON" variant={'outline'} />
+      <Button text="BUTTON" variant={'red'} size={'lg'} />
+      <Button text="BUTTON" size={'full'} />
+      <Button text="BUTTON" size={'full'} className="rounded-full" />
     </div>
   );
 }
