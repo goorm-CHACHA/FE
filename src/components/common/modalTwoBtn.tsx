4ba44babd5c8f3b54e215857d61b4cd1aa717f09
gouoy <ga0cord@gmail.com>
2025-03-04 07:49:01 +0900

    feat: 모달창 제작

--- src/components/common/modalTwoBtn.tsx
+++ src/components/common/modalTwoBtn.tsx
@@ -2,34 +2,34 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '~/utils/cn';
 import Button from './button';
-import { IoCloseSharp } from "react-icons/io5";
+import { IoCloseSharp } from 'react-icons/io5';
 import { useState } from 'react';
 const modalTwoBtnVariants = cva(
   'rounded-2xl p-3 break-words whitespace-pre-line fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black m-auto height-2xl',
-    {
-      variants: {
-        variant: {
-          system: 'bg-gray-200 text-black mr-auto rounded-bl-md',
-          btn: 'bg-white text-black mx-auto width-2xl height-2xl',
-        },
-        size: {
-          default: 'text-sm pt-16 px-14 pb-6 sm:pt-16 sm:px-16',
-          small: 'text-xs p-2',
-          large: 'text-base p-4',
-        },
+  {
+    variants: {
+      variant: {
+        system: 'bg-gray-200 text-black mr-auto rounded-bl-md',
+        btn: 'bg-white text-black mx-auto width-2xl height-2xl',
       },
-      defaultVariants: {
-        variant: 'btn',
-        size: 'default',
+      size: {
+        default: 'text-sm pt-16 px-14 pb-6 sm:pt-16 sm:px-16',
+        small: 'text-xs p-2',
+        large: 'text-base p-4',
       },
     },
+    defaultVariants: {
+      variant: 'btn',
+      size: 'default',
+    },
+  },
 );
 
-  interface ModalTwoBtnProps
+interface ModalTwoBtnProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof modalTwoBtnVariants> {
   textTitle?: string;
-  text?:string;
+  text?: string;
   textLBtn?: string;
   textRBtn?: string;
 }
@@ -47,21 +47,22 @@ const ModalTwoBtn = ({
   const [isClicked, setIsClicked] = useState(false);
   const btnCloseHandler = () => {
     setIsClicked(true);
-    // page 이동 
-  } 
+    // page 이동
+  };
   return (
-    !isClicked &&
-      <div className={cn(modalTwoBtnVariants({variant, size}))}>
-      <div className="absolute top-2 left-0 flex justify-between w-full px-4">
-        <p>{textTitle}</p>
-        <IoCloseSharp onClick={btnCloseHandler} className='cursor-pointer'/>
+    !isClicked && (
+      <div className={cn(modalTwoBtnVariants({ variant, size }))}>
+        <div className="absolute top-2 left-0 flex justify-between w-full px-4">
+          <p>{textTitle}</p>
+          <IoCloseSharp onClick={btnCloseHandler} className="cursor-pointer" />
+        </div>
+        <p className="mb-10 text-center">{text}</p>
+        <div className="flex justify-between gap-5">
+          <Button text={textLBtn} size={'sm'} onClick={btnCloseHandler} />
+          <Button text={textRBtn} size={'sm'} onClick={btnCloseHandler} />
+        </div>
       </div>
-      <p className="mb-10 text-center">{text}</p>
-      <div className='flex justify-between gap-5'>
-        <Button text={textLBtn} size={'sm'} onClick={btnCloseHandler}/>
-        <Button text={textRBtn} size={'sm'} onClick={btnCloseHandler}/>
-      </div>
-    </div>
+    )
   );
 };
 
