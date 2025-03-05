f9750acc8b237d84127be156fb9937742568a047
Gayeong Kim <155832049+gouoy@users.noreply.github.com>
2025-03-05 15:26:01 +0900

    feat: 공통 컴포넌트 multiple selection 제작 (#29)

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
 } from '~/components/common/card';
 import ChatBubble from '~/components/common/chat-bubble';
+import MultipleSelection from '~/components/common/multiple-selection';
 import Profile from '~/components/common/profile';
 
 const Home = () => {
@@ -57,6 +58,19 @@ const Home = () => {
         </Card>
       </div>
       <Profile name="홍길동" info1="1년차" info2="프론트엔드 개발자" />
+      <div className='flex gap-2 flex-wrap'>
+        <MultipleSelection>개발자</MultipleSelection>
+        <MultipleSelection variant={'pink'}>디자이너</MultipleSelection>
+        <MultipleSelection variant={'indigo'}>기획자</MultipleSelection>
+        <MultipleSelection variant={'indigo'}>개발자</MultipleSelection>
+        <MultipleSelection variant={'pink'}>디자이너</MultipleSelection>
+        <MultipleSelection>개발자</MultipleSelection>
+        <MultipleSelection variant={'indigo'}>디자이너</MultipleSelection>
+        <MultipleSelection variant={'pink'}>개발자</MultipleSelection>
+        <MultipleSelection>기획자</MultipleSelection>
+        <MultipleSelection variant={'indigo'}>디자이너</MultipleSelection>
+        <MultipleSelection variant={'pink'}>기획자</MultipleSelection>
+      </div>
       <div>
         <p>nav</p>
         <BottomNavigation />
