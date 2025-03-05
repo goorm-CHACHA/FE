e4d44dd253bcaaf51daf1d5d4427bfc6618ca9bc
JIYOON <112847202+jyooni99@users.noreply.github.com>
2025-03-05 11:26:52 +0900

    chore: radix ui 설치 (#27)
    * chore: radix ui 설치
    
    * chore: modal 파일명 변경 및 never used 문제 해결
    
    * chore: radix ui color 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import { mauve, violet, red, blackA, gray } from '@radix-ui/colors';
+
 import type { Config } from 'tailwindcss';
 
 export default {
@@ -10,6 +12,28 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        ...mauve,
+        ...violet,
+        ...red,
+        ...blackA,
+        ...gray,
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        contentShow: {
+          from: {
+            opacity: '0',
+            transform: 'translate(-50%, -48%) scale(0.96)',
+          },
+          to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
