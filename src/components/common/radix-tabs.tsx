6ee9ac7f077fe79294f9880f659f03d52a0905f5
cho-eun-young <106731103+cho-eun-young@users.noreply.github.com>
2025-03-06 17:52:16 +0900

    feat : 페이지 레이아웃 설계 - 매칭 (#32)
    * feat: 채팅 페이지 레이아웃 설계
    
    * feat: 매칭  페이지 레이아웃 설계
    
    * feat: RadixTabs Matching home으로 변경
    
    * docs: match페이지 삭제
    
    * docs: chat 페이지 top-layout으로 위치 변경
    
    * docs: 프로필 샘플 데이터 추가
    
    * docs: 프로필 id 추가
    
    * feat: match 컴포넌트 1:1,그룹 추가
    
    * chore: react-switch 설치
    
    * feat: switch 컴포넌트
    
    * style: switch 컴포넌트 style 변경
    
    * style: 스위치 삭제
    
    * feat: top-nav 컴포넌트
    
    * feat: 퀵커넥트 on off 블러처리
    
    * style: 문구 변경
    
    * docs: React.FC props로 변경
    
    * docs: React.FC props로 변경

--- src/components/common/radix-tabs.tsx
+++ src/components/common/radix-tabs.tsx
@@ -3,17 +3,18 @@ import * as Tabs from '@radix-ui/react-tabs';
 interface RadixTabsProps {
   tabLabels: string[];
   tabContents: React.ReactNode[];
+  disabled?: boolean;
 }
-
-const RadixTabs = ({ tabLabels, tabContents }: RadixTabsProps) => {
+const RadixTabs = ({ tabLabels, tabContents, disabled }: RadixTabsProps) => {
   return (
     <Tabs.Root className="flex flex-col" defaultValue={tabLabels[0]}>
       <Tabs.List className="flex bg-zinc-200 rounded-lg" aria-label="탭 목록">
         {tabLabels.map((label, index) => (
           <Tabs.Trigger
             key={`trigger-${index}`}
-            className="flex h-[45px] flex-1 cursor-pointer select-none items-center justify-center px-5 text-sm text-zinc-400 outline-none data-[state=active]:bg-black data-[state=active]:rounded-lg data-[state=active]:text-white"
+            className="flex h-[45px] flex-1 cursor-pointer select-none items-center justify-center px-5 text-sm text-zinc-400 outline-none data-[state=active]:bg-black data-[state=active]:rounded-lg data-[state=active]:text-white disabled:opacity-50 disabled:cursor-not-allowed"
             value={label}
+            disabled={disabled}
           >
             {label}
           </Tabs.Trigger>
