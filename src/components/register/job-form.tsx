31d566db9ea20a169a028379885ca05c81d59f94
jyooni <berry3891@naver.com>
2025-03-06 18:39:03 +0900

    feat: 회원 등록 - 직무 및 경력 페이지

---
+++ src/components/register/job-form.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import Select, { GroupBase } from 'react-select';
+import { Controller, useForm } from 'react-hook-form';
+import {
+  careerOptions,
+  GroupedJobOption,
+  groupedJobOptions,
+} from '~/constants/job-options';
+import Button from '~/components/common/button';
+import { useRouter } from 'next/router';
+
+const GroupLabel = (group: GroupBase<GroupedJobOption>) => {
+  return (
+    <div className="border-b pb-2 mb-2">
+      <p className="text-base">{group.label}</p>
+    </div>
+  );
+};
+
+export default function JobSelect() {
+  const { control, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    router.push('/register/');
+  });
+
+  return (
+    <div className="h-full">
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col justify-between h-full"
+      >
+        <div className="flex flex-col gap-5">
+          <Controller
+            name="job"
+            control={control}
+            rules={{ required: '직무/직책을 선택해주세요.' }}
+            render={({ field, fieldState }) => (
+              <div>
+                <p className="mb-1 font-medium text-sm">직무 / 직책</p>
+                <Select
+                  {...field}
+                  instanceId="job-select"
+                  options={groupedJobOptions}
+                  formatGroupLabel={GroupLabel}
+                  isClearable
+                />
+                {fieldState.error && (
+                  <p className="text-sm text-red-500 mt-2">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            name="career"
+            control={control}
+            rules={{ required: '경력을 선택해주세요.' }}
+            render={({ field, fieldState }) => (
+              <div>
+                <p className="mb-1 font-medium text-sm">경력</p>
+                <Select
+                  {...field}
+                  instanceId="career-select"
+                  options={careerOptions}
+                  isClearable
+                />
+                {fieldState.error && (
+                  <p className="text-sm text-red-500 mt-2">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </div>
+            )}
+          />
+        </div>
+        <Button className="py-3">다음으로</Button>
+      </form>
+    </div>
+  );
+}
