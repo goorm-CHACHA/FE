f17008f90a6a86c070d8487f6d81eb0b99424fcb
gouoy <ga0cord@gmail.com>
2025-03-06 10:45:22 +0900

    feat: 페이지 레이아웃 설계 - QR

---
+++ app/mypage/name-card-list/page.tsx
@@ -0,0 +1,31 @@
+'use client'; 
+import React, { useRef, useState } from 'react'
+import BottomNavigation from '~/components/common/bottom-nav';
+import Profile from '~/components/common/profile';
+
+const Page = () => {
+  // const DivToImage = () => {
+  //   const exportDivImageRef: any = useRef();
+  //   const [downloading, setDownloading] = useState<boolean>(false);
+  // }
+  const downloadHandler = () => {
+    console.log('het koos gemaakt');
+  }
+  return (
+   <div> 
+    <h1>저장한 명함 목록</h1>
+    <div className='flex flex-col items-center'>
+      {/* ⬇️  바로 밑에 div는 map으로 내가 저장한 카드 프린트...*/}
+      <div className='flex flex-col justify-center align-middle gap-4'>
+        <Profile name="김기획" info1="병아리 기획자" info2="웹/앱 기획" hideMatching={true}/>
+        <Profile name="이개발" info1="시니어 개발자" info2="SI 솔루션" hideMatching={true}/>
+        <Profile name="박디쟌" info1="3년차" info2="스타트업" hideMatching={true}/>
+      </div>
+    </div>
+    <div className="flex justify-center items-center m-8 p-1 min-w-[250px] max-w-[400px] bg-white text-gray-700 cursor-pointer" onClick={downloadHandler}>이미지 다운로드</div>
+    <BottomNavigation/>
+  </div>
+  )
+}
+
+export default Page; 
--- src/components/common/profile.tsx
+++ src/components/common/profile.tsx
@@ -8,18 +8,19 @@ interface ProfileProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
   info1: string;
   info2: string;
+  hideMatching?: boolean;
 }
 
-const Profile = ({ className, name, info1, info2, ...props }: ProfileProps) => {
+const Profile = ({ className, name, info1, info2, hideMatching, ...props }: ProfileProps) => {
   const [isAppliedMatching, setIsAppliedMatching] = useState(false);
-
   const clickMatchingHandler = () => {
     setIsAppliedMatching((prev) => !prev);
   };
 
+
   return (
-    <div className="rounded-2xl p-4 max-w-[90%] sm:max-w-[50%] md:max-w-[40%]  break-words relative {...props} flex bg-white text-gray-700 gap-4 py-8 justify-between item-bottom">
-      <div className="flex gap-4 items-center justify-between">
+    <div className="rounded-2xl p-4 min-w-[300px] max-w-[90%] m-auto sm:max-w-[50%] md:max-w-[40%]  break-words relative flex bg-white text-gray-700 gap-4 py-8 justify-between item-bottom">
+      <div className="flex gap-4 items-center justify-left">
         <DefaultProfile />
         <div>
           <p className="font-semibold">{name}</p>
@@ -30,21 +31,25 @@ const Profile = ({ className, name, info1, info2, ...props }: ProfileProps) => {
         </div>
       </div>
       {/* 매칭할 때 버튼 */}
-      <div className="flex flex-col gap-4">
-        <span
-          className={`h-6 flex items-center text-xs border p-2 text-zinc-500 border-zinc-500 rounded-2xl ${isAppliedMatching ? 'opacity-100' : 'opacity-0'}`}
-        >
-          ✅ 매칭 완료
-        </span>
-        <Button
-          size="sm"
-          className={'h-10 w-full align-bottom'}
-          variant={isAppliedMatching ? 'grey' : 'default'}
-          onClick={clickMatchingHandler}
-        >
-          {isAppliedMatching === true ? `신청 완료` : `매칭 신청`}
-        </Button>
-      </div>
+      {
+        !hideMatching && (
+          <div className="flex flex-col gap-4">
+            <span
+              className={`h-6 flex items-center text-xs border p-2 text-zinc-500 border-zinc-500 rounded-2xl ${isAppliedMatching ? 'opacity-100' : 'opacity-0'}`}
+            >
+              ✅ 매칭 완료
+            </span>
+            <Button
+              size="sm"
+              className={'h-10 w-full align-bottom'}
+              variant={isAppliedMatching ? 'grey' : 'default'}
+              onClick={clickMatchingHandler}
+            >
+              {isAppliedMatching === true ? `신청 완료` : `매칭 신청`}
+            </Button>
+          </div>
+        )
+      }
     </div>
   );
 };
