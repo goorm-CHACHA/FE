f53afc9727b8ff494956c79b05b0e26ac744b61d
cho-eun-young <choeunyoung224@gmail.com>
2025-03-06 14:23:42 +0900

    feat: RadixTabs Matching home으로 변경

--- app/(main-layout)/home/page.tsx
+++ app/(main-layout)/home/page.tsx
@@ -1,30 +1,37 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import Button from '~/components/common/button';
+import RadixTabs from '~/components/common/radix-tabs';
+import GroupMatching from '~/components/match/group';
+import OneToOneMatching from '~/components/match/one-to-one';
 
 const Page = () => {
-  const router = useRouter();
+  const tabLabels = ['1:1 매칭', '그룹 매칭'];
+
+  // 샘플 profiles 데이터
+  const Profiles: {
+    id: string;
+    name: string;
+    info1: string;
+    info2: string;
+  }[] = [
+    { id: '1', name: '홍길동', info1: 'Developer', info2: 'React' },
+    { id: '2', name: '김길동', info1: 'Designer', info2: 'Figma' },
+  ];
+
+  const tabContents = [
+    <OneToOneMatching key="one-to-one" profiles={Profiles} />,
+    <GroupMatching key="group" profiles={Profiles} />, // profiles 전달
+  ];
 
   return (
-    <div className="flex min-h-screen px-6 justify-center items-center flex-col gap-2">
-      <Button
-        size={'full'}
-        onClick={() => {
-          router.push('/match');
-        }}
-      >
-        네트워킹 참여여부 YES
-      </Button>
-      <Button
-        size={'full'}
-        onClick={() => {
-          router.push('/landing');
-        }}
-      >
-        네트워킹 참여여부 NO
-      </Button>
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen w-full items-center pt-52">
+        <div className="w-full md:max-w-md px-6">
+          <RadixTabs
+            tabLabels={tabLabels}
+            tabContents={tabContents}
+          ></RadixTabs>
+        </div>
+      </div>
+    </>
   );
 };
 
