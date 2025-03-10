b8ec0234bf22c5c7dbce7e07906e81a3c506c7e3
jyooni <berry3891@naver.com>
2025-03-10 23:11:07 +0900

    feat: 사전 등록 시 사전 등록 위치에 QR 코드 이미지 출력

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,28 @@
 import { useRouter } from 'next/navigation';
 import Button from '~/components/common/button';
 import { useFormStore } from '~/stores/use-form-store';
+import { QRCodeSVG } from 'qrcode.react';
 
 const Page = () => {
   const router = useRouter();
-  const { qrData, formData } = useFormStore();
+  const { qrData } = useFormStore();
 
   return (
     <div className="flex flex-col gap-4 min-h-screen px-6 justify-center items-center">
-      <Button
-        size={'full'}
-        variant={'red'}
-        onClick={() => {
-          router.push('/register');
-        }}
-      >
-        사전등록하기
-      </Button>
+      {Object.keys(qrData).length !== 0 ? (
+        <QRCodeSVG value={JSON.stringify(qrData)} />
+      ) : (
+        <Button
+          size={'full'}
+          variant={'red'}
+          onClick={() => {
+            router.push('/register');
+          }}
+        >
+          사전등록하기
+        </Button>
+      )}
+
       <Button
         size={'full'}
         onClick={() => {
@@ -27,7 +33,6 @@ const Page = () => {
       >
         네트워킹존 입장하기
       </Button>
-      <Button onClick={() => console.log(qrData, formData)}>정보 확인</Button>
     </div>
   );
 };
