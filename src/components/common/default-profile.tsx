8c11a567fa688b2a120bc3b5e271a0689d24db23
cho-eun-young <choeunyoung224@gmail.com>
2025-03-14 17:01:05 +0900

    feat: size 추가

--- src/components/common/default-profile.tsx
+++ src/components/common/default-profile.tsx
@@ -7,6 +7,7 @@ const defaultProfileVariants = cva(
   {
     variants: {
       size: {
+        xs: 'w-6 h-6',
         default: 'w-16 h-16', // 매칭 카드, 온라인 명함 저장 될 카드.....
         notification: 'h-10 w-10',
         profile: 'w-16 h-16',
