22f7e21a8e30355cc14239535c1322fda3b97e7b
cho-eun-young <choeunyoung224@gmail.com>
2025-03-18 11:49:43 +0900

    feat: chat bubble 수정

--- src/components/chat/chat-bubble.tsx
+++ src/components/chat/chat-bubble.tsx
@@ -8,9 +8,9 @@ const chatBubbleVariants = cva(
     variants: {
       variant: {
         sender:
-          'bg-[#3A3A3A] ml-auto rounded-tl-[8px] rounded-br-[8px] rounded-bl-[8px] px-4 py-2.5 text-white',
+          'bg-[#555555] ml-auto rounded-tl-[8px] rounded-br-[8px] rounded-bl-[8px] px-4 py-2.5 text-white',
         receiver:
-          'bg-[#555555] mr-auto rounded-tr-[8px] rounded-br-[8px] rounded-bl-[8px] px-4 py-2.5 text-white',
+          'bg-[#1f1f1f] mr-auto rounded-tr-[8px] rounded-br-[8px] rounded-bl-[8px] px-4 py-2.5 text-white',
         system: 'bg-yellow-100 text-black mx-auto italic rounded-lg',
       },
       size: {
