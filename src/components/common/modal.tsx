3aa0a1c10aaf9efa99ea3c7dc890d96d069880bd
cho-eun-young <choeunyoung224@gmail.com>
2025-03-19 11:16:33 +0900

    feat: 채팅방 나가기 모달 UI

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -16,7 +16,9 @@ interface ModalContent {
 }
 
 export interface ModalProps extends ModalContent {
-  triggerButtonLabel: string;
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  triggerButtonLabel?: string;
   triggerButtonVariant?: ButtonVariantProps['variant'];
 }
 
@@ -24,11 +26,12 @@ const Modal = ({
   title,
   subText,
   buttons,
+  isOpen,
+  onOpenChange,
   triggerButtonLabel,
   triggerButtonVariant,
 }: ModalProps) => {
   const initialModal = { title, subText, buttons };
-  const [isOpen, setIsOpen] = useState(false);
   const [currentModal, setCurrentModal] = useState<ModalContent>({
     title,
     subText,
@@ -43,53 +46,47 @@ const Modal = ({
   };
 
   return (
-    <>
-      <Dialog.Root
-        open={isOpen}
-        onOpenChange={(open) => {
-          setIsOpen(open);
-          if (!open) setCurrentModal(initialModal);
-        }}
-      >
+    <Dialog.Root open={isOpen} onOpenChange={onOpenChange}>
+      {triggerButtonLabel && (
         <Dialog.Trigger asChild>
           <Button variant={triggerButtonVariant}>{triggerButtonLabel}</Button>
         </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
-          <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-[#373734] p-3.5 shadow-[var(--shadow-6)] focus:outline-none data-[state=open]:animate-contentShow">
-            <Dialog.Title className="text-white text-[15px] font-semibold text-center whitespace-pre-line">
-              {currentModal.title}
-            </Dialog.Title>
-            <Dialog.Description className="text-[#b0b0b0] text-[13px] text-center whitespace-pre-line">
-              {currentModal.subText}
-            </Dialog.Description>
-            <div className="relative w-full text-center mt-4 flex gap-1.5">
-              {currentModal.buttons.map((btn, index) => {
-                if (btn.actionType === 'trigger') {
-                  return (
-                    <Button
-                      key={index}
-                      variant={btn.variant}
-                      onClick={() => handleModalContent(btn.nextModalContent!)}
-                    >
-                      {btn.label}
-                    </Button>
-                  );
-                }
-
+      )}
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-[#373734] p-3.5 shadow-[var(--shadow-6)] focus:outline-none data-[state=open]:animate-contentShow">
+          <Dialog.Title className="text-white text-[15px] font-semibold text-center whitespace-pre-line">
+            {currentModal.title}
+          </Dialog.Title>
+          <Dialog.Description className="text-[#b0b0b0] text-[13px] text-center whitespace-pre-line">
+            {currentModal.subText}
+          </Dialog.Description>
+          <div className="relative w-full text-center mt-4 flex gap-1.5">
+            {currentModal.buttons.map((btn, index) => {
+              if (btn.actionType === 'trigger') {
                 return (
-                  <Dialog.Close asChild key={index}>
-                    <Button onClick={btn.onClick} variant={btn.variant}>
-                      {btn.label}
-                    </Button>
-                  </Dialog.Close>
+                  <Button
+                    key={index}
+                    variant={btn.variant}
+                    onClick={() => handleModalContent(btn.nextModalContent!)}
+                  >
+                    {btn.label}
+                  </Button>
                 );
-              })}
-            </div>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </>
+              }
+
+              return (
+                <Dialog.Close asChild key={index}>
+                  <Button onClick={btn.onClick} variant={btn.variant}>
+                    {btn.label}
+                  </Button>
+                </Dialog.Close>
+              );
+            })}
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
--- src/components/common/top-nav.tsx
+++ src/components/common/top-nav.tsx
@@ -1,18 +1,55 @@
 'use client';
 
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import ToggleSwitch from '~/components/common/switch';
+import Modal, { ModalProps } from '~/components/common/modal';
 import { useNetworkStore } from '~/stores/use-network-store';
 import { getTopNavType } from '~/utils/get-top-nav-type';
+import { ButtonVariantProps } from '~/components/common/button';
 
 const TopNavigation = () => {
   const { isConnect, setIsConnect } = useNetworkStore();
   const pathname = usePathname();
   const { type, title } = getTopNavType(pathname);
   const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleExitChatRoom = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleConfirmExit = () => {
+    setShowModal(false);
+    router.push('/home');
+  };
+
+  const exitChatRoomModalProps: Omit<ModalProps, 'isOpen' | 'onOpenChange'> = {
+    title: '채팅방에서 나가시겠어요?',
+    subText: '채팅방에서 나가면 네트워킹이 취소돼요.',
+    buttons: [
+      {
+        label: '아니요',
+        variant: 'black-transparent',
+        actionType: 'action' as const,
+        onClick: handleCloseModal,
+      },
+      {
+        label: '네',
+        variant: 'green',
+        actionType: 'action' as const,
+        onClick: handleConfirmExit,
+      },
+    ],
+    triggerButtonLabel: '',
+    triggerButtonVariant: 'primary' as ButtonVariantProps['variant'],
+  };
 
   return (
     <div>
@@ -64,9 +101,7 @@ const TopNavigation = () => {
           <>
             <button
               className="flex items-center gap-2"
-              onClick={() => {
-                router.push('/home');
-              }}
+              onClick={handleExitChatRoom}
             >
               <p className="text-lg font-semibold text-white">채팅방</p>
               <Image
@@ -85,6 +120,13 @@ const TopNavigation = () => {
           </>
         )}
       </div>
+      {showModal && (
+        <Modal
+          {...exitChatRoomModalProps}
+          isOpen={showModal}
+          onOpenChange={setShowModal}
+        />
+      )}
     </div>
   );
 };
