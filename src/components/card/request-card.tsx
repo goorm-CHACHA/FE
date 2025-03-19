37442a25b4b56a5aaa1ef18a694aaaac37374838
gouoy <ga0cord@gmail.com>
2025-03-19 13:57:53 +0900

    refactor: 카드 컴포넌트 재분배

---
+++ src/components/card/request-card.tsx
@@ -0,0 +1,47 @@
+import CardNotifyProps from '~/types/card-notify';
+import { Card, CardBody, CardFooter, CardHeader } from '../common/card';
+import {
+  Arrow,
+  MainMsg,
+  MsgContainer,
+  SubMsg,
+  TimeAgo,
+  TimeLeft,
+} from '../notifications/notify-card';
+import DefaultProfile from '../common/default-profile';
+import Button from '../common/button';
+import Link from 'next/link';
+
+const RequestCard = ({
+  message,
+  subMessage,
+  timeStamp,
+  onAccept,
+  requester,
+}: CardNotifyProps) => {
+  return (
+    <Card className="w-full max-w-3xl  shadow-md rounded-lg mx-auto">
+      <CardHeader className="flex justify-between items-center mb-4">
+        <TimeLeft text="매칭요청" />
+        <Link href={`/user-info/${requester}`}>
+          <Arrow />
+        </Link>
+      </CardHeader>
+      <CardBody className={`flex justify-left items-center gap-4 mb-4`}>
+        <DefaultProfile size="notification" />
+        <MsgContainer>
+          <MainMsg message={message} />
+          <SubMsg subMessage={subMessage} />
+        </MsgContainer>
+      </CardBody>
+      <CardFooter className="flex justify-between items-center align-top">
+        <TimeAgo timestamp={timeStamp || Date.now()} />
+        <Button onClick={onAccept} size="sm">
+          수락
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default RequestCard;
--- src/components/notifications/notify-card.tsx
+++ src/components/notifications/notify-card.tsx
@@ -1,20 +1,20 @@
 'use client';
 import React from 'react';
 import { GrFormNext } from 'react-icons/gr';
-import DefaultProfile from '../common/default-profile';
-import Button from '../common/button';
 import { MessageType } from '../../stores/use-notify-store';
 import { useRouter } from 'next/navigation';
 import { formatDistanceToNow } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import type { Locale } from 'date-fns';
-import { Card, CardBody, CardFooter, CardHeader } from '../common/card';
+import NormalCard from '../card/normal-card';
+import RequestCard from '../card/request-card';
+import { NotifyUser } from '~/types/notify-user';
 interface NotifyProps {
   messageData: {
     message: string;
     subMessage: string;
     status?: MessageType;
-    requester?: { id: string; name?: string } | null;
+    requester?: NotifyUser;
     chatRoomId?: number;
     timeStamp?: number;
   };
@@ -43,6 +43,7 @@ const NotifyCard = ({ messageData }: NotifyProps) => {
           subMessage={subMessage}
           timeStamp={timeStamp}
           onAccept={handleAccept}
+          requester={messageData.requester?.id}
         />
       );
     } else {
@@ -56,67 +57,11 @@ const NotifyCard = ({ messageData }: NotifyProps) => {
     }
   };
 
-  return (
-    <div>
-      <div>{notificationsToShow()}</div>
-    </div>
-  );
+  return notificationsToShow();
 };
 
 export default NotifyCard;
 
-//  💳 CARDS
-interface CardProps {
-  message: string;
-  subMessage: string;
-  timeStamp?: number;
-  onAccept?: () => void; // "수락" 버튼 클릭 핸들러
-}
-
-const RequestCard = ({
-  message,
-  subMessage,
-  timeStamp,
-  onAccept,
-}: CardProps) => {
-  return (
-    <Card className="w-full max-w-3xl  shadow-md rounded-lg mx-auto">
-      <CardHeader className="flex justify-between items-center mb-3 ">
-        <TimeLeft />
-        <Arrow />
-      </CardHeader>
-      <CardBody className={`flex justify-left items-center gap-4 mb-4`}>
-        <DefaultProfile />
-        <MsgContainer>
-          <MainMsg message={message} />
-          <SubMsg subMessage={subMessage} />
-        </MsgContainer>
-      </CardBody>
-      <CardFooter className="flex justify-between items-center align-top">
-        <TimeAgo timestamp={timeStamp || Date.now()} />
-        <Button onClick={onAccept}>수락</Button>
-      </CardFooter>
-    </Card>
-  );
-};
-const NormalCard = ({ message, subMessage, timeStamp }: CardProps) => {
-  return (
-    <Card>
-      <CardHeader className="flex justify-between items-center mb-3">
-        <PushAlarm />
-      </CardHeader>
-      <CardBody className={`flex justify-left items-center gap-4 mb-4`}>
-        <MsgContainer>
-          <MainMsg message={message} />
-          <SubMsg subMessage={subMessage} />
-        </MsgContainer>
-      </CardBody>
-      <CardFooter className="flex justify-between items-center align-top">
-        <TimeAgo timestamp={timeStamp || Date.now()} />
-      </CardFooter>
-    </Card>
-  );
-};
 // COMPONENTS
 // ▼ ******* 📦 container
 // 🔸 Msg 컨테이너 - 안에 message, subMessage 선택 취하
@@ -125,7 +70,7 @@ interface ContainerProps {
   className?: string;
 }
 
-const MsgContainer = ({ children }: ContainerProps) => {
+export const MsgContainer = ({ children }: ContainerProps) => {
   return (
     <div className="flex flex-col justify-center gap-2 break-words">
       {children}
@@ -135,7 +80,7 @@ const MsgContainer = ({ children }: ContainerProps) => {
 // ⏏️ container 📦 ********
 
 // ✼✼ 화살표 이모티콘
-const Arrow = () => {
+export const Arrow = () => {
   return (
     <div>
       <GrFormNext />
@@ -143,32 +88,35 @@ const Arrow = () => {
   );
 };
 
+interface TimeLeftProps {
+  text: string;
+}
 // ✼✼매칭요청 뱃지 시간 3분 가정하고 시간 흘러감..
-const TimeLeft = () => {
+export const TimeLeft = ({ text }: TimeLeftProps) => {
   return (
-    <p className="inline-block bg-slate-900 text-sm px-4 py-2 rounded-3xl">
-      매칭요청
+    <p className="inline-block bg-slate-900 px-3 py-1 text-body-sm rounded-3xl">
+      {text}
       <span className="inline-block ml-2 text-red-600">2:59</span>
     </p>
   );
 };
 
 // ✼✼푸시알림 뱃지
-const PushAlarm = () => {
+export const PushAlarm = () => {
   return (
-    <p className="inline-block bg-slate-900 px-4 py-2 text-sm rounded-3xl">
+    <p className="inline-block bg-slate-900 px-3 py-1 text-body-sm rounded-3xl">
       푸시알람
     </p>
   );
 };
 
 // ✼✼ Main Message , Sub Message
-const MainMsg = ({ message }: { message: string }) => {
-  return <p className="font-semibold line-clamp-2">{message}</p>;
+export const MainMsg = ({ message }: { message: string }) => {
+  return <p className="font-semibold text-body-md line-clamp-1">{message}</p>;
 };
 
-const SubMsg = ({ subMessage }: { subMessage: string }) => {
-  return <span className="text-gray-500 line-clamp-2">{subMessage}</span>;
+export const SubMsg = ({ subMessage }: { subMessage: string }) => {
+  return <span className="text-body-sm text-gray-500">{subMessage}</span>;
 };
 
 // ✼✼알림 시간.
@@ -176,16 +124,10 @@ interface TimeAgoProps {
   timestamp?: number;
 }
 
-const TimeAgo = ({ timestamp = Date.now() }: TimeAgoProps) => {
+export const TimeAgo = ({ timestamp = Date.now() }: TimeAgoProps) => {
   const timeAgo = formatDistanceToNow(new Date(timestamp), {
     addSuffix: true,
     locale: ko as unknown as Locale,
   });
   return <span>{timeAgo}</span>;
 };
-
-// ************ 일단 만들어본... 매칭 쪽으로?
-
-// const Title =  () => {
-//   return <p className="text-lg font-bold">관심사</p>
-// }
