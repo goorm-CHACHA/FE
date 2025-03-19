a6827e4685ba115f9cfd28fff9c0addce0f73cf2
gouoy <ga0cord@gmail.com>
2025-03-19 14:04:32 +0900

    feat: 아코디언 ui 수정

--- src/components/common/accordion/accordion.tsx
+++ src/components/common/accordion/accordion.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import './style.css';
 import * as Accordion from '@radix-ui/react-accordion';
-
+import Image from 'next/image';
 interface AccordionProps {
   className?: string;
   children?: React.ReactNode;
@@ -27,7 +27,7 @@ interface AccordionItemProps {
 const AccordionItem = ({ className, children, value }: AccordionItemProps) => (
   <Accordion.Item
     className={classNames(
-      'mt-px overflow-hidden p-spacing-20 box-border text-left text-base bg-[color:var(--primitive-color-gray-warm-800)] bg-[#222222] text-white font-body1-normal-16 first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px] focus-within:shadow-mauve12',
+      'mt-px overflow-hidden w-full p-spacing-20 box-border text-left text-base bg-[color:var(--primitive-color-gray-warm-800)] bg-[#222222] text-white font-body1-normal-16 first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px] focus-within:shadow-mauve12 flex-shrink-0',
       className,
     )}
     value={value}
@@ -50,18 +50,20 @@ const AccordionTrigger = ({
   <Accordion.Header className="flex">
     <Accordion.Trigger
       className={classNames(
-        'h-20 group flex flex-1 cursor-default items-center justify-between bg-[#222222] px-5 text-[15px] leading-none text-white shadow-[0_1px_0] shadow-mauve6 outline-none',
+        'h-20 group flex flex-1 w-full max-w-full cursor-default items-center justify-between bg-gray-warm-800 px-5 text-[15px] leading-none text-white shadow-[0_1px_0] shadow-mauve6 outline-none',
         className,
       )}
       {...props}
     >
       {children}
-      <p
-        className="text-white transition-transform duration-300 ease-[cubic-bezier(0.87,_0,_0.13,_1)] group-data-[state=open]:rotate-180"
-        aria-hidden
-      >
-        ^
-      </p>
+      <Image
+        src="/assets/Arrow.svg"
+        alt="Arrow Icon"
+        width={12}
+        height={12}
+        unoptimized={true}
+        className="transition-transform duration-300 ease-[cubic-bezier(0.87,_0,_0.13,_1)]"
+      />
     </Accordion.Trigger>
   </Accordion.Header>
 );
@@ -78,7 +80,7 @@ const AccordionContent = React.forwardRef<
 >(({ children, className, ...props }, forwardedRef) => (
   <Accordion.Content
     className={classNames(
-      'overflow-hidden bg-[#222222] text-[15px] data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown',
+      'overflow-hidden w-full bg-[#222222] min-h-[50px] text-[15px] data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown flex-shrink-0',
       className,
       'AccordionContent',
     )}
