3d8d07ebec093e03e5a6f5c7577563186848ef5f
gouoy <ga0cord@gmail.com>
2025-03-19 13:44:50 +0900

    feat: 그룹 매칭 상태 바 컴포넌트 생성

--- src/components/match/status-for-group.tsx
+++ src/components/match/status-for-group.tsx
@@ -19,12 +19,9 @@ const StatusForGroup = ({ variants, className }: StatusForGroupProps) => {
   return (
     // <div className="w-full relative rounded-radius-8 bg-gray-900 flex flex-row items-center justify-start py-spacing-8 px-spacing-12 box-border gap-spacing-6 text-left text-xs text-white font-body5-normal-b-12">
     <div
-      className={`w-full relative rounded-2xl bg-gray-900 flex flex-row items-center justify-start py-2 px-4 box-border gap-2 text-left text-xs text-white font-body5-normal-b-12 mt-2 ${className}`}
+      className={`inline-flex relative rounded-2xl bg-gray-900 items-center justify-start py-2 px-4 box-border gap-2 text-left text-xs text-white font-body5-normal-b-12 mt-2 ${className}`}
     >
-      <div
-        className={`w-2 relative rounded-[50%] bg-green-500 h-2 ${circleColorMapping[variants]} `}
-      />
-      <div className="flex flex-row items-center justify-start"></div>
+      <div className={`w-2 h-2 rounded-full ${circleColorMapping[variants]}`} />
       <div className="relative tracking-[-0.02em] leading-[140%] font-semibold">
         {statusLabels[variants]}
       </div>
