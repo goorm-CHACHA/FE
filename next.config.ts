debb28e49ce1adaca017ed0307b6c1bdf0d93f51
jyooni <berry3891@naver.com>
2025-03-25 23:44:36 +0900

    chore: next.config.ts 변경

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+
   async rewrites() {
     return [
       {
