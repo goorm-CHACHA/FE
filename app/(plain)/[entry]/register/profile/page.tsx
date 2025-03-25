a470303f7a790bcc4d90ad6220025e56c97f0d0b
jyooni <berry3891@naver.com>
2025-03-25 15:03:28 +0900

    feat: 사전, 현장 등록

--- app/(plain)/[entry]/register/profile/page.tsx
+++ app/(plain)/[entry]/register/profile/page.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ProfileForm from '~/components/register/profile-form';
 import RegisterTemplete from '~/components/register/register-templete';
 
-const RegisterPage = () => {
+interface PageProps {
+  params: {
+    entry: string;
+  };
+}
+
+const RegisterPage = async ({ params }: PageProps) => {
+  const entry = params.entry; // 나중에 fix
+
   return (
     <RegisterTemplete
-      title="🎉 컨퍼런스 사전 등록"
+      title={`🎉 컨퍼런스 ${entry === 'pre' ? '사전' : '현장'} 등록`}
       subtitle={`행사 기간 동안 세션 출입, 네트워킹, 온라인 명함 교환 등 다양한 활동을 원활하게 이용하시려면 사전 등록이 필요합니다.`}
     >
       <ProfileForm />
