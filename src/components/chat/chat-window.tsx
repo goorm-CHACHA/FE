f39577cdfa748b8bdcd63c302f9ea4e7299472ee
cho-eun-young <choeunyoung224@gmail.com>
2025-03-25 15:17:17 +0900

    feat: develop merge

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -105,16 +105,14 @@ const ChatWindow = ({
 
         {/* 채팅 메시지 영역 */}
         <div className="p-4 ">
-          {
-             messages.map((message) => (
-              <ChatBubble
+          {messages.map((message) => (
+            <ChatBubble
               key={message.createTime}
               message={message.message} // 메시지 내용
               createTime={message.createTime}
               senderName={message.senderName}
             />
-            ))
-          }
+          ))}
         </div>
       </div>
     </div>
