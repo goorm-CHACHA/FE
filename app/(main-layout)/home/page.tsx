86282a0c31a83abd9ae5c8aaed9fae25b2819945
gouoy <ga0cord@gmail.com>
2025-03-25 16:14:56 +0900

    feat: 로그인한 유저 식별 위한 유저 타입 추가

--- app/(main-layout)/home/page.tsx
+++ app/(main-layout)/home/page.tsx
@@ -8,34 +8,32 @@ import GroupMatching from '~/components/match/group';
 import OneToOneMatching from '~/components/match/one-to-one';
 import { useNetworkStore } from '~/stores/use-network-store';
 import { mockUserData } from '~/components/mypage/mock-user-data';
-import { useEffect } from 'react';
-import axios from 'axios';
 import { useUserStore } from '~/stores/use-user-store';
+import { UserData } from '~/types/user.types';
+
 const Page = () => {
   const { isConnect } = useNetworkStore();
-  const { users, setUsers } = useUserStore();
-
-  // ✅ 모든 유저 불러오기
+  const { users, setUsers, loggedInUser } = useUserStore();
+  console.log(users);
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}api/users/all`,
-        );
+        const res = await api.get<UserData[]>('/api/users/all');
+        const filteredUsers = loggedInUser
+          ? res.data.filter((user) => user.id !== loggedInUser.id)
+          : (res.data as UserData[]);
 
-        setUsers(res.data);
-        useUserStore.getState().setUsers(res.data);
+        setUsers(filteredUsers);
       } catch (error) {
         console.error('유저 목록 불러오기 실패:', error);
       }
     };
 
     fetchUsers();
-  }, [setUsers]);
+  }, [setUsers, loggedInUser]);
 
   const tabLabels = ['1:1 매칭', '그룹 매칭'];
   const tabContents = [
-    <OneToOneMatching key="one-to-one" profiles={users} />,
     <OneToOneMatching key="one-to-one" profiles={users} />,
     <GroupMatching key="group" profiles={mockUserData} />,
   ];
--- src/stores/use-user-store.ts
+++ src/stores/use-user-store.ts
@@ -1,18 +1,21 @@
 // stores/use-user-store.ts
 import { create } from 'zustand';
 import { UserData } from '~/types/user.types';
+
 interface UserStore {
   users: UserData[];
   selectedUser: UserData | null;
+  loggedInUser: UserData | null;
   setUsers: (users: UserData[]) => void;
   setSelectedUser: (user: UserData) => void;
+  // setLoggedInUser: (user: UserData) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   users: [],
   selectedUser: null,
+  loggedInUser: null,
   setUsers: (users) => set({ users }),
   setSelectedUser: (user) => set({ selectedUser: user }),
+  // setLoggedInUser: (user) => set({ loggedInUser: user }), // ✅ 추가
 }));
-
-// use-user-store.ts
