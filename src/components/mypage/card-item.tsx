318f4a031b78663216dda0f5a7fba804cf674b29
cho-eun-young <choeunyoung224@gmail.com>
2025-03-26 23:50:58 +0900

    feat: 1:1 매칭 프로필 이미지

--- src/components/mypage/card-item.tsx
+++ src/components/mypage/card-item.tsx
@@ -2,17 +2,23 @@ import { Card, CardBody } from '~/components/common/card';
 import DefaultProfile from '~/components/common/default-profile';
 import CardBasic from '~/components/common/card-basic';
 import { QRCodeType } from '~/types/form';
+import { UserData } from '~/types/user.types';
 
 interface CardItemProps {
   user: QRCodeType;
+  userData: UserData;
   onClick: () => void;
 }
 
-const CardItem = ({ user, onClick }: CardItemProps) => {
+const CardItem = ({ userData, user, onClick }: CardItemProps) => {
   return (
     <Card key={user.id} onClick={onClick}>
       <CardBody className="flex gap-[10px] p-[10px]">
-        <DefaultProfile size="nameCard" />
+        <DefaultProfile
+          size="nameCard"
+          jobValue={userData.jobValue}
+          interestJobValue={userData.interestJobValue}
+        />
         <div className="flex flex-col items-start justify-center gap-[6px]">
           <CardBasic userId={user.name} />
           <div className="flex items-center gap-2 text-body-sm text-[#b0b0b0]">
