728425b240aa029303295f6187732bcb7b4deb05
kimjunghwannn <aa2002dcks@naver.com>
2025-03-26 16:38:10 +0900

    feat: 매칭 신청 상단 고정 및 알림 기능에 따른 모달 변경 (#123)

--- app/(main-layout)/home/page.tsx
+++ app/(main-layout)/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import api from '~/utils/api/api';
 
 import RadixTabs from '~/components/common/radix-tabs';
@@ -13,15 +13,143 @@ import { UserData } from '~/types/user.types';
 
 const Page = () => {
   const { isConnect } = useNetworkStore();
-  const { users, setUsers, loggedInUser } = useUserStore();
+  const { users, setUsers } = useUserStore();
+  const [loggedInUser, setLoggedInUser] = useState<UserData | null>(null);
+  const [websocket, setWebSocket] = useState<WebSocket | null>(null);
+
   console.log(users);
+  console.log(loggedInUser);
+
+  // 채팅 요청 알림 UI 표시
+  function showChatRequestNotification(
+    message: string,
+    requesterId: number,
+    receiverId: number,
+  ) {
+    const notificationElement = document.createElement('div');
+    notificationElement.innerHTML = `
+      <p>${message}</p>
+      <button id="acceptButton">수락</button>
+      <button id="rejectButton">거절</button>
+    `;
+    document.body.appendChild(notificationElement); // 메시지를 UI에 추가
+
+    // 수락 버튼 클릭 시 acceptChat 호출
+    const acceptButton = notificationElement.querySelector('#acceptButton')!;
+    acceptButton.addEventListener('click', () =>
+      acceptChat(requesterId, receiverId),
+    );
+
+    // 거절 버튼 클릭 시 rejectChat 호출
+    const rejectButton = notificationElement.querySelector('#rejectButton')!;
+    rejectButton.addEventListener('click', () => rejectChat(requesterId));
+  }
+
+  // 채팅 방 수락 함수
+  function acceptChat(requesterId: number, receiverId: number) {
+    const chatRequest = {
+      requesterId: requesterId,
+      receiverId: receiverId,
+    };
+
+    // API 호출: 채팅 방 수락
+    fetch('/private-chatroom/accept', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(chatRequest),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('채팅 방 수락 성공:', data);
+        // 수락 후 UI 업데이트 등 필요한 작업 추가
+        // 수락 후에 챗으로 가야함 방 번호와 함께
+      })
+      .catch((error) => {
+        console.error('채팅 방 수락 실패:', error);
+      });
+  }
+
+  // 채팅 방 거절 함수
+  function rejectChat(requesterId: number) {
+    // API 호출: 채팅 방 거절
+    fetch(`/private-chatroom/reject?requesterId=${requesterId}`, {
+      method: 'GET',
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('채팅 방 거절 성공');
+          // 거절 후 UI 업데이트 등 필요한 작업 추가
+        }
+      })
+      .catch((error) => {
+        console.error('채팅 방 거절 실패:', error);
+      });
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      `ws://${process.env.NEXT_PUBLIC_WS_API_URL}/notifications`,
+    );
+    setWebSocket(ws);
+
+    // WebSocket 연결이 열리면
+    ws.onopen = () => {
+      console.log('WebSocket 연결됨');
+    };
+    const fetchLoggedInUser = async () => {
+      try {
+        const res = await api.get('/api/users/mypage', {});
+        const data = await res.data;
+        setLoggedInUser(data); // 로그인된 유저 정보 상태 설정
+      } catch (error) {
+        console.error('로그인된 유저 정보 불러오기 실패:', error);
+      }
+    };
+
+    ws.onmessage = (event) => {
+      const notificationData = JSON.parse(event.data); // 메시지 파싱
+
+      // 메시지 타입이 'request'일 때
+      if (notificationData.messageType === 'request') {
+        const requesterId = notificationData.requesterId;
+        const receiverId = notificationData.receiverId;
+        const message = notificationData.message;
+
+        // UI에 채팅 요청 알림을 띄운다.
+        showChatRequestNotification(message, requesterId, receiverId);
+      }
+
+      if (notificationData.messageType === 'reject') {
+        const message = notificationData.message;
+
+        alert(message); // '채팅 요청이 거절되었습니다' 등 메시지 띄우기
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await api.get<UserData[]>('/api/users/all');
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_HTTP_API_URL}users/all`,
+          {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+          },
+        );
+
+        // res.json()을 호출해서 JSON 데이터를 가져와야 함
+        const data = await res.json();
+
         const filteredUsers = loggedInUser
-          ? res.data.filter((user) => user.id !== loggedInUser.id)
-          : (res.data as UserData[]);
+          ? data.filter(
+              (user: UserData) => user.nickName !== loggedInUser.nickName,
+            )
+          : (data as UserData[]);
 
         setUsers(filteredUsers);
       } catch (error) {
@@ -47,7 +175,6 @@ const Page = () => {
           disabled={!isConnect}
         />
       </div>
-
       {!isConnect && (
         <div className="absolute inset-0 top-0 w-full max-w-3xl h-full bg-black bg-opacity-75 backdrop-filter backdrop-blur-sm flex items-center justify-center pb-[92px]">
           <p className="text-white text-lg font-semibold text-center">
