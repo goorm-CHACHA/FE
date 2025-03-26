4e2dcba35f9ae5418ad808a95107da62beddc1e8
gouoy <ga0cord@gmail.com>
2025-03-26 10:14:49 +0900

    feat: 프로필 이미지 적용

--- src/components/common/default-profile.tsx
+++ src/components/common/default-profile.tsx
@@ -1,9 +1,48 @@
 import { cva, VariantProps } from 'class-variance-authority';
-import Image from 'next/image';
 import { cn } from '~/utils/cn';
+import BirdIcon from '../common/quick-network/welcome-icon/bird'
+import CatIcon from '../common/quick-network/welcome-icon/cat';
+import DogIcon from '../common/quick-network/welcome-icon/dog';
+import JellyFishIcon from '../common/quick-network/welcome-icon/jelly-fish';
+import KoalaIcon from '../common/quick-network/welcome-icon/koala';
+import MonkeyIcon from '../common/quick-network/welcome-icon//monkey';
+import OtterIcon from '../common/quick-network/welcome-icon/otter';
+import QuokkaIcon from '../common/quick-network/welcome-icon/quokka';
+import RabbitIcon from '../common/quick-network/welcome-icon/rabbit';
+import SnailIcon from '../common/quick-network/welcome-icon/snail';
+import SnowmanIcon from '../common/quick-network/welcome-icon/snowman';
+import WhaleIcon from '../common/quick-network/welcome-icon/whale';
+import { useEffect, useState } from 'react';
+
+const icons = [
+  BirdIcon,
+  CatIcon,
+  DogIcon,
+  JellyFishIcon,
+  KoalaIcon,
+  MonkeyIcon,
+  OtterIcon,
+  QuokkaIcon,
+  RabbitIcon,
+  SnailIcon,
+  SnowmanIcon,
+  WhaleIcon,
+];
+
+const colors = [
+  '#30B6FD', // Blue
+  '#1EDC93', // Green
+  '#FF9257', // Orange
+  '#FF84EF', // Pink
+  '#856BFF', // Purple
+  '#FE7777', // Red
+  '#FFFFFF', // White
+  '#FFF280', // Yellow
+];
+
 
 const defaultProfileVariants = cva(
-  'relative rounded-full overflow-hidden bg-slate-400',
+  'relative rounded-full overflow-hidden bg-gray-neutral-900',
   {
     variants: {
       size: {
@@ -23,27 +62,36 @@ const defaultProfileVariants = cva(
   },
 );
 
+const RandomIcon = () => {
+  const [SelectedIcon, setSelectedIcon] = useState<React.FC<React.SVGProps<SVGSVGElement>> | null>(null);
+  const [color, setColor] = useState<string>('#000');
+
+  useEffect(() => {
+    const icon = icons[Math.floor(Math.random() * icons.length)];
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    setSelectedIcon(() => icon);
+    setColor(color);
+  }, []);
+
+  if (!SelectedIcon) return null;
+
+  return (
+    <SelectedIcon
+      className="w-full h-full fill-current"
+      style={{ fill: color, color }}
+    />
+  );
+};
 interface DefaultProfileProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof defaultProfileVariants> {
   imgSrc?: string;
 }
 
-const DefaultProfile = ({ imgSrc, size, className }: DefaultProfileProps) => {
+const DefaultProfile = ({ size, className }: DefaultProfileProps) => {
   return (
     <div className={cn(defaultProfileVariants({ size }), className)}>
-      {imgSrc ? (
-        <Image
-          src={imgSrc}
-          alt="프로필 이미지"
-          fill
-          className="object-contain"
-        />
-      ) : (
-        <div className="w-full h-full flex items-center justify-center bg-gray-300">
-          <span className="text-white text-xs">No Image</span>
-        </div>
-      )}
+       <RandomIcon />
     </div>
   );
 };
