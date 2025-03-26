c2511abb4348aa8b4e0509fe33faf9231b9b8e2f
jyooni <berry3891@naver.com>
2025-03-26 22:24:07 +0900

    feat: 명함 구현

--- src/components/mypage/detail-card.tsx
+++ src/components/mypage/detail-card.tsx
@@ -4,8 +4,13 @@ import { useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import { Dialog } from 'radix-ui';
 import { QRCodeType } from '~/types/form';
+
 import RepeatIcon from '~/assets/svgs/repeat.svg';
 import ScannerIcon from '~/assets/svgs/scanner.svg';
+import UnderLeftDeco from '~/assets/svgs/graphic-under-left.svg';
+import UpRightDeco from '~/assets/svgs/graphic-up-right.svg';
+
+import { getHashColor, getIconByJob } from '~/utils/get-profile-style';
 
 interface DetailCardProps {
   user: QRCodeType | null;
@@ -17,42 +22,68 @@ const DetailCard = ({ user, isShowQR = false }: DetailCardProps) => {
   const handleToggle = () => setShowQR((prev) => !prev);
 
   return (
-    <div
-      className="perspective-1000 w-[315px] h-[468px] cursor-pointer"
-      onClick={handleToggle}
-    >
+    <>
+      <Dialog.Title className="text-white text-sm font-medium flex justify-between w-[215px] absolute top-[-60px] left-1/2 -translate-x-1/2 z-10 pointer-events-none">
+        <span className="translate-y-[110%] inline-flex gap-1 items-center ㅋ-">
+          <RepeatIcon />
+          카드를 뒤집어 명함을 교환하세요!
+        </span>
+      </Dialog.Title>
       <div
-        className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d rounded-2xl shadow-md ${
-          showQR ? 'rotate-y-180' : ''
-        }`}
+        className="perspective-1000 w-[315px] h-[468px] cursor-pointer overflow-hidden rounded-2xl"
+        onClick={handleToggle}
       >
-        <FrontCard user={user} />
-        <BackCard user={user} />
+        <div
+          className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d rounded-2xl shadow-md ${
+            showQR ? 'rotate-y-180' : ''
+          }`}
+        >
+          <FrontCard user={user} />
+          <BackCard user={user} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const FrontCard = ({ user }: { user: QRCodeType | null }) => {
+  const color = getHashColor(user?.job.value ?? '');
+  const Icon = getIconByJob(user?.job.value);
+
+  return (
+    <div className="absolute w-full h-full backface-hidden bg-stone-900 py-16 px-10 rounded-2xl">
+      <UnderLeftDeco
+        className="absolute left-[-2px] bottom-[0px]"
+        width={121}
+        height={104}
+        fill={color}
+      />
+      <UpRightDeco
+        className="absolute right-[0px] top-[0px]"
+        fill={color}
+        width={102}
+        height={87}
+      />
+      <Icon
+        className="absolute bottom-[20px] right-[20px]"
+        style={{ color }}
+        width={136}
+        height={136}
+      />
+      <div className="flex flex-col items-start h-full">
+        <p className="text-white text-4xl font-semibold leading-[43.2px] mb-10">
+          {user?.name}
+        </p>
+        <div className="w-full space-y-3 font-['Pretendard'] font-light">
+          <InfoRow label="직무/직책" value={user?.job.value} />
+          <InfoRow label="소속" value={user?.affiliation} />
+          <InfoRow label="이메일" value={user?.email} />
+          <InfoRow label="휴대폰 번호" value={user?.contactInfo} />
+        </div>
       </div>
     </div>
   );
 };
-
-const FrontCard = ({ user }: { user: QRCodeType | null }) => (
-  <div className="absolute w-full h-full backface-hidden bg-stone-900 py-16 px-10 rounded-2xl">
-    <div className="flex flex-col items-start h-full">
-      <p className="text-white text-4xl font-semibold leading-[43.2px] mb-10">
-        {user?.name}
-      </p>
-
-      <div className="w-full space-y-3 font-['Pretendard'] font-light">
-        <InfoRow label="직무/직책" value={user?.job.value} />
-        <InfoRow label="소속" value={user?.affiliation} />
-        <InfoRow label="이메일" value={user?.email} />
-        <InfoRow label="휴대폰 번호" value={user?.contactInfo} />
-      </div>
-    </div>
-    <Dialog.Title className="text-white text-sm font-medium flex justify-between w-[215px] absolute bottom-[-40px]">
-      <RepeatIcon />
-      카드를 뒤집어 명함을 교환하세요!
-    </Dialog.Title>
-  </div>
-);
 
 const BackCard = ({ user }: { user: QRCodeType | null }) => (
   <div className="absolute w-full h-full rotate-y-180 backface-hidden bg-stone-900 py-16 px-10 rounded-2xl">
@@ -65,7 +96,7 @@ const BackCard = ({ user }: { user: QRCodeType | null }) => (
         level="H"
         marginSize={2}
       />
-      <Dialog.Title className="text-white text-sm font-medium flex justify-between absolute bottom-[-40px] w-[160px]">
+      <Dialog.Title className="text-white text-sm font-medium flex justify-between w-[160px]">
         <ScannerIcon />
         QR코드를 스캔해주세요
       </Dialog.Title>
