4f2193a297649ce280f10f9c7d0bf695a3742c8c
jyooni <berry3891@naver.com>
2025-03-26 10:22:24 +0900

    feat: Merge branch

--- app/(main-layout)/user-info/[userId]/page.tsx
+++ app/(main-layout)/user-info/[userId]/page.tsx
@@ -33,7 +33,7 @@ const UserInfoPage = () => {
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <div className="px-5">
+      <div className="w-full px-5">
         <div className="bg-gray-neutral-800 w-full py-7 px-4 rounded-[12px] flex flex-col gap-4 items-center">
           <ProfileImportant
             userData={user}
