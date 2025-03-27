7edf7069d7529628eff34ff7de864806fc752a79
gouoy <ga0cord@gmail.com>
2025-03-27 12:15:25 +0900

    feat: table api 작성

---
+++ src/utils/api/table.ts
@@ -0,0 +1,58 @@
+import api from './api';
+
+export async function requestTable(chatRoomId: number) {
+  try {
+    await api.post(`/api/networking-table/apply/${chatRoomId}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function cancelTable(chatRoomId: number) {
+  try {
+    await api.post(`/api/networking-table/apply/${chatRoomId}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function startNetworking(userId: number, tableNumber: string) {
+  try {
+    await api.post('/api/networking-table/start', {
+      userId,
+      tableNumber,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function endsNetwork(userId: number, tableNumber: string) {
+  try {
+    await api.post('/api/networking-table/end',{
+      userId,
+      tableNumber,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function reserveTable() {
+  try {
+    await api.post('/api/reservation/create');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function consentReservation(chatRoomId: number, userId: number) {
+  try {
+    await api.post('/api/reservation/consent',{
+      chatRoomId,
+      userId,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
