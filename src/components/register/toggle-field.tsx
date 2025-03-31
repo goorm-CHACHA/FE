cc5556be3f7a1c83a3fff46b3b2be81a06a51fdf
cho-eun-young <choeunyoung224@gmail.com>
2025-03-31 15:35:04 +0900

    feat: 토글 컴포넌트

--- src/components/register/toggle-field.tsx
+++ src/components/register/toggle-field.tsx
@@ -18,6 +18,7 @@ interface ToggleFieldProps<T extends FieldValues> {
   maxSelection?: number;
   toggleVariants?: 'black' | 'primary' | 'primary-small';
   className?: string;
+  onChange?: (interest: string) => void;
 }
 
 const ToggleField = <T extends FieldValues>({
@@ -30,6 +31,7 @@ const ToggleField = <T extends FieldValues>({
   maxSelection = 1,
   toggleVariants = 'primary',
   className,
+  onChange,
 }: ToggleFieldProps<T>) => {
   const { setError, setValue } = useFormContext();
 
