e9fa7cb4348f5f074d8d85c650dc11d5742ea68a
jyooni <berry3891@naver.com>
2025-03-31 11:32:31 +0900

    chore: router 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 'use client';
-
-import { useRouter } from 'next/navigation';
-import Button from '~/components/common/button';
 import LoginForm from '~/components/login-form';
 
 const Page = () => {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col gap-4 min-h-screen px-6 justify-center items-center">
       <LoginForm />
